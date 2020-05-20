71f2a84079273f96dbd55989070423cdffe0afa9
Jakub Hrášek <hrasekj@gmail.com>
2020-05-20 20:34:22 +0200

    i3 lock script

---
+++ .i3/lock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ICON=$HOME/.i3/lock.png
+TMPBG=/tmp/screen.png
+
+scrot $TMPBG
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+
+i3lock -e -u -i $TMPBG
+rm $TMPBG
